c20dcaf5ceeab41bd9e4888773f3d5b520c37612
Lucas Bueno <79279663+lucasviamaquinas@users.noreply.github.com>
2023-01-13 16:02:51 -0300

    feat: componente menu

---
+++ src/views/app/MainTemplate/components/Menu/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import { BarChart2, BarChart3, BookOpen, LineChart, LogOut, MessageSquare, Star, User, Users } from "lucide-react";
+
+import Nav from "../Nav";
+import Link from "../Link";
+
+import "./styles.css";
+
+const Menu = () => {
+
+  return (
+    <div className="main-template__menu">
+      <Nav>
+        <Link icon={<MessageSquare />} title="Conversas" to="/" active={true} />
+        <Link icon={<LineChart />} title="Dashboard" to="/" />
+        <Link icon={<Users />} title="Meus clientes">
+          <Link title="Novos" />
+          <Link title="Disponíveis" />
+        </Link>
+      </Nav>
+
+      <Nav>
+        <Link icon={<BarChart2 />} title="Ranking" to="/" />
+        <Link icon={<Star />} title="Lotes favoritados" to="/" />
+        <Link icon={<BookOpen />} title="Relatório de acessos" to="/" />
+      </Nav>
+
+      <Nav>
+        <Link icon={<User />} title="Minha conta" to="/" />
+        <Link icon={<LogOut />} title="Sair" to="/" />
+      </Nav>
+    </div >
+  );
+}
+
+export default Menu;
