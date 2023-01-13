b641cfb0d3b0be1dd4df11e75eead22efd2eb06f
Lucas Bueno <79279663+lucasviamaquinas@users.noreply.github.com>
2023-01-13 16:05:46 -0300

    feat: componente logo

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Logo = () => {
+
+  return (
+    <span className="text-center text-white fs-16">
+      gvm<span className="text-bold text-primary-color">desk</span>
+    </span>
+  );
+}
+
+export default Logo;
