6bc1c02a2728a38d91ee5db95b2bd2001c91f00f
Lucas Bueno <79279663+lucasviamaquinas@users.noreply.github.com>
2023-01-17 16:10:05 -0300

    feat: mock de chats

---
+++ src/views/app/Chats/chats.js
@@ -0,0 +1,72 @@
+const chats = [
+  {
+    id: 1,
+    name: "Carlos Eduardo Daniel Assunção",
+    current_message: "Poderia me passar informações sobre o leilão?",
+    hour: "12:32",
+    unread_messages: "0",
+    profile_picture:
+      "https://cdn.dribbble.com/users/1786866/screenshots/13992097/media/c461eeae4d4b523c9d3bab7c66264916.png?compress=1&resize=400x300",
+    is_new: true,
+    is_vacation: false,
+    is_transfered: false,
+  },
+  {
+    id: 2,
+    name: "Samuel José Ruan Mendes",
+    current_message: "Estou interessado em uma máquina",
+    hour: "12:32",
+    unread_messages: "3",
+    profile_picture:
+      "https://cdn.dribbble.com/users/230875/screenshots/12078079/media/7ba8ec4a42b529dcbbc695ce0dd07a4a.jpg?compress=1&resize=400x300&vertical=top",
+    is_new: true,
+    is_vacation: false,
+    is_transfered: false,
+  },
+  {
+    id: 3,
+    name: "Carla Aline de Paula",
+    current_message: "TRATOR JD 6600 ANO 1999 LOTE (#87699)",
+    hour: "12:32",
+    unread_messages: "4",
+    profile_picture:
+      "https://cdn.dribbble.com/users/230875/screenshots/12078079/media/1aab4f168be4969bd7f99d3734b71261.jpg?compress=1&resize=400x300&vertical=top",
+    is_new: true,
+    is_vacation: false,
+    is_transfered: false,
+  },
+  {
+    id: 4,
+    name: "Roberto Osvaldo da Silva",
+    current_message: "Não quero mais nenhum trator, obrigado",
+    hour: "12:32",
+    unread_messages: "1",
+    profile_picture: "https://pbs.twimg.com/media/EbRZ0YAU8AEY7KJ.jpg",
+    is_new: false,
+    is_vacation: true,
+  },
+  {
+    id: 5,
+    name: "Marcelo Yuri Corte Real",
+    current_message: "Não entendi o que você quis dizer",
+    hour: "12:32",
+    unread_messages: "1",
+    profile_picture: "https://cdna.artstation.com/p/assets/images/images/050/172/070/large/mathraw-mathou-chx.jpg?1654208026",
+    is_new: false,
+    is_vacation: false,
+    is_transfered: true,
+  },
+  {
+    id: 6,
+    name: "Carlos Hugo Julio da Cunha",
+    current_message: "Eu sou o Trunks do futuro",
+    hour: "12:32",
+    unread_messages: "1",
+    profile_picture: "https://cdnb.artstation.com/p/assets/images/images/050/106/473/smaller_square/mathraw-pp-pfa-v2.jpg?1654076570",
+    is_new: false,
+    is_vacation: false,
+    is_transfered: true,
+  },
+];
+
+export default chats;
