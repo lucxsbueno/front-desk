b089a889511b0a8faaee30cf5f648f227d1e4a6f
Lucas Bueno <79279663+lucasviamaquinas@users.noreply.github.com>
2023-01-17 16:13:18 -0300

    feat: section dos chats (filtros)

---
+++ src/views/app/Chats/components/Section/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "./styles.css";
+
+const Section = (props) => {
+  const { title, length, children } = props;
+
+  return (
+    <div className="chat__section">
+      <div className="chat__heading">
+        <h2 className="chat__title">{title}</h2>
+        <h2 className="chat__title">{length}</h2>
+      </div>
+
+      <div className="chat__items">{children}</div>
+    </div>
+  );
+};
+
+Section.defaultProps = {
+  title: "Item item item",
+  length: 0,
+};
+
+export default Section;
