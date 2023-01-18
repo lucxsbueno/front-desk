ff59e61206a2c56ec2d4c53f64075d774e6d5560
Lucas Bueno <79279663+lucasviamaquinas@users.noreply.github.com>
2023-01-18 17:38:07 -0300

    feat: adições

--- src/views/Routes.js
+++ src/views/Routes.js
@@ -5,21 +5,29 @@ import AccessReports from "./app/AccessReports";
 
 //views
 import Chats from "./app/Chats";
+import Chat from "./app/Chats/Chat";
 import FavLots from "./app/FavLots";
 import Ranking from "./app/Ranking";
 import Dashboard from "./app/Dashboard";
 import MyAccount from "./app/MyAccount";
+import ChatIndex from "./app/Chats/ChatIndex";
 import CustomersNew from "./app/CustomersNew";
 import MainTemplate from "./app/MainTemplate";
 import RouteWithoutIndex from "./app/RouteWithoutIndex";
 import CustomersAvailable from "./app/CustomersAvailable";
+import Information from "./app/Information";
 
 export const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<MainTemplate />}>
         <Route index element={<RouteWithoutIndex to="/conversas" />} />
-        <Route path="conversas" element={<Chats />} />
+        <Route path="conversas" element={<Chats />}>
+          <Route index element={<ChatIndex />} />
+          <Route path=":id" element={<Chat />}>
+            <Route path="informacoes" element={<Information />} />
+          </Route>
+        </Route>
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="clientes/novos" element={<CustomersNew />} />
         <Route path="clientes/disponiveis" element={<CustomersAvailable />} />
