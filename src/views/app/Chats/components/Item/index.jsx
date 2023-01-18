5bb6ef8cb4eadec2b1565398dc36c9e56976686d
Lucas Bueno <79279663+lucasviamaquinas@users.noreply.github.com>
2023-01-18 17:36:53 -0300

    feat: fix e adições

--- src/views/app/Chats/components/Item/index.jsx
+++ src/views/app/Chats/components/Item/index.jsx
@@ -7,12 +7,14 @@ import "./styles.css";
 const Item = (props) => {
   const { chat } = props;
 
-  const { id, name, current_message, profile_picture, unread_messages, hour } = chat;
+  const { id, name, current_message, profile_picture, unread_messages, hour } =
+    chat;
 
-  const readMessage = unread_messages == 0 ? "chat__current-messages--read" : "";
+  const readMessage =
+    unread_messages == 0 ? "chat__current-messages--read" : "";
 
   return (
-    <NavLink to={`/conversas/${id}`} className="chat__item">
+    <NavLink to={`/conversas/${id}`} state={{ data: { title: name, profile_picture: profile_picture } }} className="chat__item">
       <div className="chat__left">
         <div className="chat__avatar">
           <img src={profile_picture} alt="" className="chat__img" />
@@ -26,11 +28,11 @@ const Item = (props) => {
         </div>
 
         <div className="d-flex flex-row justify-content-space-between align-items-center">
-          <span className={`chat__current-message ${readMessage}`.trim()}>{current_message}</span>
+          <span className={`chat__current-message ${readMessage}`.trim()}>
+            {current_message}
+          </span>
           {unread_messages > 0 && (
-            <span className="chat__unread-message">
-              {unread_messages}
-            </span>
+            <span className="chat__unread-message">{unread_messages}</span>
           )}
         </div>
 
