bb262ceb87fe64d4ff96b162a7c095fa32e9f880
Lucas Bueno <79279663+lucasviamaquinas@users.noreply.github.com>
2023-01-20 13:07:34 -0300

    feat: componente card de documentos

---
+++ src/views/app/Information/components/DocumentsCard/index.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+import Card from "../../../../../components/Card";
+
+import "./styles.css";
+
+const DocumentsCard = () => {
+  return (
+    <div className="x-p-15 y-p-15 pt-0">
+      <Card>
+        <table className="docs__table">
+          <tbody className="docs__body">
+
+            <tr className="docs__row">
+              <td className="docs__table-data">
+                <div className="d-flex flex-row align-items-center">
+                  <div className="doc__item d-flex flex-row align-items-center">
+                    <div className="doc__bg">
+                      <img className="doc__icon" src="" alt="" />
+                    </div>
+                    <div className="d-flex flex-column">
+                      <span className="doc__title">Identidade</span>
+                      <div className="d-flex flex-row">
+                        <span className="doc__size">100KB</span>
+                        <span className="doc__type">PDF</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </td>
+            </tr>
+
+            <tr className="docs__row">
+              <td className="docs__table-data">
+                <div className="d-flex flex-row align-items-center">
+                  <div className="doc__item d-flex flex-row align-items-center">
+                    <div className="doc__bg">
+                      <img className="doc__icon" src="" alt="" />
+                    </div>
+                    <div className="d-flex flex-column">
+                      <span className="doc__title">Comprovante</span>
+                      <div className="d-flex flex-row">
+                        <span className="doc__size">100KB</span>
+                        <span className="doc__type">PDF</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </td>
+            </tr>
+
+            <tr className="docs__row">
+              <td className="docs__table-data">
+                <div className="d-flex flex-row align-items-center">
+                  <div className="doc__item d-flex flex-row align-items-center">
+                    <div className="doc__bg">
+                      <img className="doc__icon" src="" alt="" />
+                    </div>
+                    <div className="d-flex flex-column">
+                      <span className="doc__title">Documentação</span>
+                      <div className="d-flex flex-row">
+                        <span className="doc__size">100KB</span>
+                        <span className="doc__type">PDF</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </td>
+            </tr>
+
+          </tbody>
+        </table>
+      </Card>
+    </div>
+  );
+};
+
+export default DocumentsCard;
