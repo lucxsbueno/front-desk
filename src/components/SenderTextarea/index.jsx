80272fd02588fc5487184d86fce69fa274821d8e
Lucas Bueno <79279663+lucasviamaquinas@users.noreply.github.com>
2023-01-23 17:41:28 -0300

    feat: textarea de envio de mensagem

---
+++ src/components/SenderTextarea/index.jsx
@@ -0,0 +1,20 @@
+import React, { useRef } from "react";
+
+import "./styles.css";
+
+const SenderTextarea = () => {
+  const textAreaRef = useRef(null);
+
+  const onChangeHandler = (e) => {
+    const target = e.target;
+    textAreaRef.current.style.height = "44px";
+    textAreaRef.current.style.height = `${target.scrollHeight}px`;
+    // props.onChange(e);
+  }
+
+  return (
+    <textarea ref={textAreaRef} onChange={onChangeHandler} rows={1} type="text" placeholder="Escreva uma mensagem para Marcelo" className="sender__input" />
+  );
+};
+
+export default SenderTextarea;
