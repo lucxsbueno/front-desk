315ac4059fb3f8c7da18ed815d33dfff29c53d4a
Lucas Bueno <79279663+lucasviamaquinas@users.noreply.github.com>
2023-01-25 16:47:05 -0300

    feat: mock

--- src/views/app/Chats/Chat/components/ChatBody/index.jsx
+++ src/views/app/Chats/Chat/components/ChatBody/index.jsx
@@ -1,19 +1,125 @@
+import { ChevronDown } from "lucide-react";
 import React from "react";
+import Dropdown from "../../../../../../components/Dropdown";
+import DropdownLink from "../../../../../../components/DropdownLink";
+import DropdownButtonMessage from "../DropdownButtonMessage";
+
+import Message from "../Message";
+import Messages from "../Messages";
+import TextMessage from "../TextMessage";
 
 import "./styles.css";
 
 const ChatBody = () => {
   return (
     <div className="chat__inner">
+      <Messages>
 
-      <div className="messages">
+        <Message>
+          <TextMessage>
+            Lorem ipsum dolor sit amet, consectetur. Praesent diam ante, scelerisque.
+          </TextMessage>
+          <Dropdown button={
+            <DropdownButtonMessage>
+              <ChevronDown size={20} />
+            </DropdownButtonMessage>
+          }>
+            <DropdownLink to="/channels/new">New channel</DropdownLink>
+            <DropdownLink to="/">Configurations</DropdownLink>
+            <DropdownLink to="/dashboard">Dashboard</DropdownLink>
+          </Dropdown>
+        </Message>
 
-        <div className="message">
+        <Message className="message--me">
+          <Dropdown button={
+            <DropdownButtonMessage>
+              <ChevronDown size={20} />
+            </DropdownButtonMessage>
+          }>
+            <DropdownLink to="/channels/new">New channel</DropdownLink>
+            <DropdownLink to="/">Configurations</DropdownLink>
+            <DropdownLink to="/dashboard">Dashboard</DropdownLink>
+          </Dropdown>
+          <TextMessage>
+            Lorem ipsum dolor sit amet, consectetur. Praesent diam ante, scelerisque. Lorem ipsum dolor sit amet, consectetur. Praesent diam ante, scelerisque.
+          </TextMessage>
+        </Message>
 
-        </div>
+        <Message className="message--me">
+          <Dropdown button={
+            <DropdownButtonMessage>
+              <ChevronDown size={20} />
+            </DropdownButtonMessage>
+          }>
+            <DropdownLink to="/channels/new">New channel</DropdownLink>
+            <DropdownLink to="/">Configurations</DropdownLink>
+            <DropdownLink to="/dashboard">Dashboard</DropdownLink>
+          </Dropdown>
+          <TextMessage>
+            Lorem ipsum dolor sit amet, consectetur.
+          </TextMessage>
+        </Message>
 
-      </div>
+        <Message>
+          <TextMessage>
+            Lorem ipsum dolor sit amet, consectetur. Praesent diam ante, scelerisque.
+          </TextMessage>
+          <Dropdown button={
+            <DropdownButtonMessage>
+              <ChevronDown size={20} />
+            </DropdownButtonMessage>
+          }>
+            <DropdownLink to="/channels/new">New channel</DropdownLink>
+            <DropdownLink to="/">Configurations</DropdownLink>
+            <DropdownLink to="/dashboard">Dashboard</DropdownLink>
+          </Dropdown>
+        </Message>
 
+        <Message>
+          <TextMessage>
+            Lorem ipsum dolor sit amet, consectetur. Praesent diam ante, scelerisque. . Praesent diam ante, scelerisque. Praesent diam ante, scelerisque
+          </TextMessage>
+          <Dropdown button={
+            <DropdownButtonMessage>
+              <ChevronDown size={20} />
+            </DropdownButtonMessage>
+          }>
+            <DropdownLink to="/channels/new">New channel</DropdownLink>
+            <DropdownLink to="/">Configurations</DropdownLink>
+            <DropdownLink to="/dashboard">Dashboard</DropdownLink>
+          </Dropdown>
+        </Message>
+
+        <Message>
+          <TextMessage>
+            Lorem ipsum dolor sit amet, consectetur. Praesent diam ante, scelerisque.. Praesent diam ante, scelerisque
+          </TextMessage>
+          <Dropdown button={
+            <DropdownButtonMessage>
+              <ChevronDown size={20} />
+            </DropdownButtonMessage>
+          }>
+            <DropdownLink to="/channels/new">New channel</DropdownLink>
+            <DropdownLink to="/">Configurations</DropdownLink>
+            <DropdownLink to="/dashboard">Dashboard</DropdownLink>
+          </Dropdown>
+        </Message>
+
+        <Message>
+          <TextMessage>
+            Lorem ipsum dolor sit amet, consectetur.
+          </TextMessage>
+          <Dropdown button={
+            <DropdownButtonMessage>
+              <ChevronDown size={20} />
+            </DropdownButtonMessage>
+          }>
+            <DropdownLink to="/channels/new">New channel</DropdownLink>
+            <DropdownLink to="/">Configurations</DropdownLink>
+            <DropdownLink to="/dashboard">Dashboard</DropdownLink>
+          </Dropdown>
+        </Message>
+      </Messages>
     </div>
   );
 };
