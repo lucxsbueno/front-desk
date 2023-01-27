145a42320a0f989a2e92fae37ea86f47b4231ea8
Lucas Bueno <79279663+lucasviamaquinas@users.noreply.github.com>
2023-01-27 17:48:53 -0300

    update: mock agora com vídeo

--- src/views/app/Chats/Chat/mock.js
+++ src/views/app/Chats/Chat/mock.js
@@ -4,85 +4,131 @@ const messages = [
     user_id: 0,
     type: "text",
     body: "Lorem ipsum dolor sit amet, consectetur. Praesent diam ante, scelerisque.",
-    timestamp: "12:32"
+    timestamp: "12:32",
   },
   {
     id: 2,
     user_id: 0,
     type: "image",
-    body: "Lorem ipsum dolor sit amet",
-    timestamp: "12:32"
+    body: "https://cdn.photoswipe.com/photoswipe-demo-images/photos/1/img-2500.jpg",
+    timestamp: "12:32",
   },
   {
     id: 3,
     user_id: 0,
     type: "video",
-    body: "Lorem ipsum dolor sit amet",
-    timestamp: "12:32"
+    body: {
+      srcUrl: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4",
+      title: "what_car_can_you_get_for_a_grand.mp4",
+      time_duration: "23:13"
+    },
+    timestamp: "12:32",
   },
   {
     id: 4,
     user_id: 0,
     type: "document",
     body: "Lorem ipsum dolor sit amet",
-    timestamp: "12:32"
+    timestamp: "12:32",
   },
   {
     id: 5,
     user_id: 0,
     type: "audio",
     body: "Lorem ipsum dolor sit amet",
-    timestamp: "12:32"
+    timestamp: "12:32",
   },
   {
     id: 6,
     user_id: 0,
     type: "sticker",
     body: "Lorem ipsum dolor sit amet",
-    timestamp: "12:32"
+    timestamp: "12:32",
   },
   {
     id: 7,
     user_id: 34,
     type: "text",
     body: "Lorem ipsum dolor sit amet, consectetur. Praesent diam ante, scelerisque. Lorem ipsum dolor sit amet, consectetur. Praesent diam ante, scelerisque.",
-    timestamp: "12:32"
+    timestamp: "12:32",
   },
   {
     id: 8,
     user_id: 34,
     type: "text",
     body: "Lorem ipsum dolor sit amet, consectetur.",
-    timestamp: "12:32"
+    timestamp: "12:32",
   },
   {
     id: 9,
     user_id: 0,
     type: "text",
     body: "Lorem ipsum dolor sit amet, consectetur. Praesent diam ante, scelerisque.",
-    timestamp: "12:32"
+    timestamp: "12:32",
   },
   {
     id: 10,
     user_id: 0,
     type: "text",
     body: "Lorem ipsum dolor sit amet, consectetur. Praesent diam ante, scelerisque. Praesent diam ante, scelerisque. Praesent diam ante, scelerisque",
-    timestamp: "12:32"
+    timestamp: "12:32",
   },
   {
     id: 11,
     user_id: 0,
     type: "text",
     body: "Lorem ipsum dolor sit amet, consectetur. Praesent diam ante, scelerisque",
-    timestamp: "12:32"
+    timestamp: "12:32",
   },
   {
     id: 12,
     user_id: 0,
     type: "text",
     body: "Lorem ipsum dolor sit amet, consectetur.",
-    timestamp: "12:32"
-  }
+    timestamp: "12:32",
+  },
+  {
+    id: 13,
+    user_id: 0,
+    type: "video",
+    body: {
+      srcUrl: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",
+      title: "for_bigger_meltdowns.mp4",
+      time_duration: "23:13"
+    },
+    timestamp: "12:32",
+  },
+  {
+    id: 14,
+    user_id: 34,
+    type: "video",
+    body: {
+      srcUrl: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+      title: "bigger_joyrides.mp4",
+      time_duration: "3:13"
+    },
+    timestamp: "12:32",
+  },
+  {
+    id: 15,
+    user_id: 34,
+    type: "image",
+    body: "https://images.unsplash.com/photo-1485550409059-9afb054cada4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+  },
+  {
+    id: 16,
+    user_id: 34,
+    type: "text",
+    body: "Lorem ipsum dolor sit amet. Praesent diam ante, scelerisque. Praesent diam ante, scelerisque",
+    timestamp: "12:32",
+  },
+  {
+    id: 17,
+    user_id: 34,
+    type: "text",
+    body: "Lorem ipsum dolor sit amet. Praesent diam ante, scelerisque",
+    timestamp: "12:32",
+  },
 ];
 
 export default messages;
