d0dd0f13505c416811836e4060dec7cbb582c1ed
Lucas Bueno <79279663+lucasviamaquinas@users.noreply.github.com>
2023-01-27 17:48:26 -0300

    refactor: mudanças nos componentes

--- src/views/app/Chats/Chat/components/ChatBody/index.jsx
+++ src/views/app/Chats/Chat/components/ChatBody/index.jsx
@@ -12,20 +12,18 @@ import "./styles.css";
 
 import mock from "../../mock";
 import ImageMessage from "../ImageMessage";
+import VideoMessage from "../VideoMessage";
 
 const ChatBody = () => {
   return (
     <div className="chat__inner">
       <Messages>
-
         {mock.map((item, index) => {
           switch (item.type) {
             case "text": {
               return (
                 <Message key={index} message={item}>
-                  <TextMessage hour={item.timestamp}>
-                    {item.body}
-                  </TextMessage>
+                  <TextMessage hour={item.timestamp}>{item.body}</TextMessage>
                   <Dropdown
                     button={
                       <DropdownButtonMessage>
@@ -45,35 +43,29 @@ const ChatBody = () => {
               return (
                 <Message key={index} message={item}>
                   <ImageMessage
-                  galleryID="gallery"
-                  images={[
-                    {
-                      largeURL:
-                        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/1/img-2500.jpg",
-                      thumbnailURL:
-                        "https://cdn.photoswipe.com/photoswipe-demo-images/photos/1/img-200.jpg",
-                      width: 1875,
-                      height: 2500,
-                    }
-                  ]}
+                    galleryID="gallery"
+                    images={[
+                      {
+                        largeURL: item.body,
+                        thumbnailURL: item.body,
+                        width: 1875,
+                        height: 2500,
+                      }
+                    ]}
                   />
-                  <Dropdown
-                    button={
-                      <DropdownButtonMessage>
-                        <ChevronDown size={20} />
-                      </DropdownButtonMessage>
-                    }
-                  >
-                    <DropdownLink to="/channels/new">New channel</DropdownLink>
-                    <DropdownLink to="/">Configurations</DropdownLink>
-                    <DropdownLink to="/dashboard">Dashboard</DropdownLink>
-                  </Dropdown>
                 </Message>
-              )
+              );
+            }
+
+            case "video": {
+              return (
+                <Message key={index} message={item}>
+                  <VideoMessage videoMetadata={item.body} />
+                </Message>
+              );
             }
           }
         })}
-
       </Messages>
     </div>
   );
