c00fdd8f78576cd65ea7b98ce70503ca987a1196
Lucas Bueno <79279663+lucasviamaquinas@users.noreply.github.com>
2023-01-30 17:35:39 -0300

    refactor: logo

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
-const Logo = () => {
+const Logo = (props) => {
+  const { className } = props;
+
   return (
-    <span className="text-center text-white fs-16">
+    <span className={`text-center text-white fs-16 ${className}`.trim()}>
       gvm<span className="text-bold text-primary-color">desk</span>
     </span>
   );
+};
+
+Logo.defaultProps = {
+  className: "",
 };
 
 export default Logo;
