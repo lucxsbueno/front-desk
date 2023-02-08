1e1cd711e1ecc7ba5043c45594ffd6d4ebf18dd9
Lucas Bueno <79279663+lucasviamaquinas@users.noreply.github.com>
2023-02-08 15:42:25 -0300

    feat: nova página histórico de ligações

---
+++ src/views/app/MyAccount/pages/CallsHistory/index.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+import { PhoneIncoming, PhoneMissed } from "lucide-react";
+
+import "./styles.css";
+
+const CallsHistory = () => {
+  return (
+    <div className="calls container">
+      <div className="row d-flex justify-content-center">
+        <div className="col-md-10 pt-15">
+          <ul className="calls__menu">
+            <div className="calls__divider" />
+
+            <li className="calls__item calls__item--incoming-call">
+              <div className="w-100 d-flex flex-row">
+                <div className="calls__icon">
+                  <PhoneIncoming />
+                </div>
+                <div className="">
+                  <p className="calls__date">16:34, 8 de abr 2023</p>
+                  <p className="calls__info">O cliente André Max Dorneles não atendeu a chamada.</p>
+                  <p className="calls__info">Nota: Lorem ipsum dolor sit amet.</p>
+                </div>
+              </div>
+            </li>
+
+            <li className="calls__item">
+              <div className="w-100 d-flex flex-row">
+                <div className="calls__icon">
+                  <PhoneMissed />
+                </div>
+                <div className="">
+                  <p className="calls__date">16:34, 8 de abr 2023</p>
+                  <p className="calls__info">O cliente André Max Dorneles não atendeu a chamada.</p>
+                  <p className="calls__info">Nota: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+              </div>
+            </li>
+
+            <li className="calls__item">
+              <div className="w-100 d-flex flex-row">
+                <div className="calls__icon">
+                  <PhoneMissed />
+                </div>
+                <div className="">
+                  <p className="calls__date">16:34, 8 de abr 2023</p>
+                  <p className="calls__info">O cliente André Max Dorneles não atendeu a chamada.</p>
+                  <p className="calls__info">Nota: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+              </div>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CallsHistory;
