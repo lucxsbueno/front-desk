055e46ab13f9c33898137bbc8b219dfc30c42730
Lucas Bueno <79279663+lucasviamaquinas@users.noreply.github.com>
2023-02-08 15:44:25 -0300

    feat: nova página de presets

---
+++ src/views/app/MyAccount/pages/Presets/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./styles.css";
+
+const Presets = () => {
+  return (
+    <div className="container">
+      <div className="row d-flex justify-content-center">
+        <div className="col-md-10 pt-15">
+          Presets
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Presets;
