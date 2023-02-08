9e182feba052074d2c8536ec191a8c18452a4df7
Lucas Bueno <79279663+lucasviamaquinas@users.noreply.github.com>
2023-02-08 15:44:34 -0300

    feat: nova página de tags

---
+++ src/views/app/MyAccount/pages/Tags/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./styles.css";
+
+const Tags = () => {
+  return (
+    <div className="container">
+      <div className="row d-flex justify-content-center">
+        <div className="col-md-10 pt-15">
+          Tags
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tags;
