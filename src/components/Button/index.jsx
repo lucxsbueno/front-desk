b934896b05ec9128025e0309265e208d60ced492
Lucas Bueno <79279663+lucasviamaquinas@users.noreply.github.com>
2023-02-09 17:44:18 -0300

    refactor: button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,13 +3,17 @@ import React from "react";
 import "./styles.css";
 
 const Button = (props) => {
-  const { onClick, children } = props;
+  const { onClick, children, className } = props;
 
   return (
-    <button className="form__bttn" onClick={onClick}>
+    <button className={`form__bttn ${className}`.trim()} onClick={onClick}>
       {children}
     </button>
   );
+};
+
+Button.defaultProps = {
+  className: ""
 };
 
 export default Button;
